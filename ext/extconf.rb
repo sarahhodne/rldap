6e53888f6870928b28326affa26a1719ab0af419
Henrik Hodne <dvyjones@binaryhex.com>
2010-09-27 18:41:45 -0700

    Updated extconf to fail if libraries are missing

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,9 +1,7 @@
 require 'mkmf'
 
-have_library('ldap')
-have_library('lber')
-
-have_header('ldap.h')
-have_header('lber.h')
+unless have_library('ldap') && have_library('lber') && have_header('ldap.h') && have_header('lber.h')
+  abort 'You need to have libldap and liblber installed.'
+end
 
 create_makefile('ldap')
